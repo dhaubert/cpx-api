4db302e8c19ef666f5ea37e46021fcf34a007ee1
Douglas Haubert <dhaubert.ti@gmail.com>
2019-08-06 00:14:18 -0300

    Alterada estrutura do database para utilizar variáveis de ambiente

--- src/app/database.js
+++ src/app/database.js
@@ -2,21 +2,40 @@ const admin = require("firebase-admin");
 
 class Database {
   constructor() {
-    this.serviceAccount = require("../../google-key.json");
-    this.url = "https://ww-w-data.firebaseio.com";
+    // try {
+      this.serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+      this.url = process.env.FIREBASE_URL;
+    // } catch (err) {
+    //   console.log(`Please, provide GOOGLE_APPLICATION_CREDENTIALS as an enviroment variable (${err})`)
+    // }
   }
-  init(){
+
+  init() {
     admin.initializeApp({
       credential: admin.credential.cert(this.serviceAccount),
       databaseURL: this.url
     });
   }
+
   async readValue(username) {
     const db = admin.database();
     let ref = db.ref(`wallets/${username}`);
     let snapshot = await ref.once("value");
     return snapshot.val();
   }
+
+  async newPair(username, currentBalance) {
+    //push
+    //https://firebase.google.com/docs/database/admin/save-data?authuser=0
+  }
+
+  async update(username, currentBalance) {
+    //set
+    //transaction
+    //https://firebase.google.com/docs/database/admin/save-data?authuser=0
+  }
+
+
 }
 
 module.exports = new Database();
