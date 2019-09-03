de66952f2e5dec7609ff88633c18d1f4e9f5896e
Douglas Haubert <dhaubert.ti@gmail.com>
2019-09-03 00:42:30 -0300

    Parse da chave privada do google para facilitar no Heroku

--- src/app/models/database.js
+++ src/app/models/database.js
@@ -10,7 +10,7 @@ class Database {
         "type": "service_account",
         "project_id": process.env.GOOGLE_PROJECT_ID,
         "private_key_id": process.env.GOOGLE_PVT_KEY_ID,
-        "private_key": process.env.GOOGLE_PVT_KEY,
+        "private_key": process.env.GOOGLE_PVT_KEY.replace(/\\n/g, '\n'),
         "client_email": process.env.GOOGLE_CLIENT_EMAIL,
         "client_id": process.env.GOOGLE_CLIENT_ID,
         "auth_uri": process.env.GOOGLE_AUTH_URI,
