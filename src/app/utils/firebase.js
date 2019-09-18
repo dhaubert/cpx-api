be5a9fb9e7a7076b193a8e58cd7a39d30b636623
Douglas Haubert <dhaubert.ti@gmail.com>
2019-09-18 20:14:53 -0300

    Replacing now the right key.

--- src/app/utils/firebase.js
+++ src/app/utils/firebase.js
@@ -7,8 +7,8 @@ class FirebaseProvider {
       this.serviceAccount = {
         type: "service_account",
         project_id: process.env.GOOGLE_PROJECT_ID,
-        private_key_id: process.env.GOOGLE_PVT_KEY_ID.replace(/\\n/g, '\n'),
-        private_key: process.env.GOOGLE_PVT_KEY,
+        private_key_id: process.env.GOOGLE_PVT_KEY_ID,
+        private_key: process.env.GOOGLE_PVT_KEY.replace(/\\n/g, '\n'),
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
         client_id: process.env.GOOGLE_CLIENT_ID,
         auth_uri: process.env.GOOGLE_AUTH_URI,
